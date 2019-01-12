13f2248c543fa5d3e1b5679f625f4e3e586f4f47
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-12 17:49:03 +0200

    initial commit

---
+++ 01.Stadium Income/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace _01.Stadium_Income
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numberOfSectors = int.Parse(Console.ReadLine());
+            int capacityStadium = int.Parse(Console.ReadLine());
+            double pricePerTicket = double.Parse(Console.ReadLine());
+
+            double incomePerSector = (capacityStadium * pricePerTicket) / numberOfSectors;
+            double totalSum = capacityStadium * pricePerTicket;
+
+            double moneyForCharity = (totalSum - (incomePerSector * 0.75)) / 8;
+
+            Console.WriteLine($"Total income - {totalSum:F2} BGN");
+            Console.WriteLine($"Money for charity - {moneyForCharity:F2} BGN");
+        }
+    }
+}
