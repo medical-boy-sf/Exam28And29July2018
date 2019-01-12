73285c049893175a7d9213df99c9cd6449e41f06
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-12 17:54:52 +0200

    Beer and Chips

---
+++ 02.BeerAndChips/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _02.BeerAndChips
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = Console.ReadLine();
+            double budget = double.Parse(Console.ReadLine());
+            int numberBeers = int.Parse(Console.ReadLine());
+            int numberPacketsChips = int.Parse(Console.ReadLine());
+
+            double sumBeers = numberBeers * 1.2;
+            double pricePacketsChips = sumBeers * 0.45;
+            double sumPacketsChips = Math.Ceiling(pricePacketsChips * numberPacketsChips);
+            double totalSum = sumBeers + sumPacketsChips;
+
+            if (budget >= totalSum)
+            {
+                Console.WriteLine($"{name} bought a snack and has {(budget - totalSum):F2} leva left.");
+            }
+
+            else
+            {
+                Console.WriteLine($"{name} needs {(totalSum - budget):F2} more leva!");
+            }
+        }
+    }
+}
