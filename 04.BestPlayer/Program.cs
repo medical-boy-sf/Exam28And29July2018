6cdfccc25620b62b8f814ef25ba879e5b39f59e7
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-12 18:02:26 +0200

    Best Player

---
+++ 04.BestPlayer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _04.BestPlayer
+{
+    class Program
+    {
+        static void Main()
+        {
+            string namePlayer = String.Empty;
+            string goals = String.Empty;
+            int goalsInt = 0;
+            int maxNum = int.MinValue;
+            string bestPlayer = String.Empty;
+
+            while (true)
+            {
+                namePlayer = Console.ReadLine();
+                if (namePlayer == "END") break;
+                goals = Console.ReadLine().ToLower();
+                goalsInt = int.Parse(goals);
+
+                if (goalsInt > maxNum)
+                {
+                    maxNum = goalsInt;
+                    bestPlayer = namePlayer;
+                }
+
+                if (goalsInt >= 10) break;
+            }
+
+            Console.WriteLine($"{bestPlayer} is the best player!");
+
+            if (maxNum >= 3)
+            {
+                Console.WriteLine($"He has scored {maxNum} goals and made a hat-trick !!!");
+            }
+
+            else
+            {
+                Console.WriteLine($"He has scored {maxNum} goals.");
+            }
+        }
+    }
+}
