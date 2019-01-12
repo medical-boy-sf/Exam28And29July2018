c861a2089645e8e5619153b9ddc8e6b73d083237
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-12 18:35:35 +0200

    Game Info

--- 05.GameInfo/Program.cs
+++ 05.GameInfo/Program.cs
@@ -6,7 +6,17 @@ namespace _05.GameInfo
     {
         static void Main()
         {
+            string nameTeam = Console.ReadLine();
+            int numberGames = int.Parse(Console.ReadLine());
+            int totalTime = 0;
 
+            for (int i = 0; i < numberGames; i++)
+            {
+                int timeGame = int.Parse(Console.ReadLine());
+                totalTime += timeGame;
+            }
+
+            Console.WriteLine($"{nameTeam} has played {totalTime} minutes. Average minutes per game: {((double)totalTime / numberGames):F2}");
         }
     }
 }
