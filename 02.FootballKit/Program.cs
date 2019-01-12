cfd08c11fcaea0a83f5863f65c8aaf0692ba3440
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-12 17:56:32 +0200

    Football Kit

---
+++ 02.FootballKit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02.FootballKit
+{
+    class Program
+    {
+        static void Main()
+        {
+            double priceTShirt = double.Parse(Console.ReadLine());
+            double sumNeededForBall = double.Parse(Console.ReadLine());
+
+            double priceShorts = 0.75 * priceTShirt;
+            double priceSocks = 0.2 * priceShorts;
+            double priceButtons = 2 * (priceTShirt + priceShorts);
+            double totalSum = priceTShirt + priceShorts + priceSocks + priceButtons;
+            totalSum = totalSum - totalSum * 0.15;
+
+            if (totalSum >= sumNeededForBall)
+            {
+                Console.WriteLine("Yes, he will earn the world-cup replica ball!");
+                Console.WriteLine($"His sum is {totalSum:F2} lv.");
+            }
+
+            else
+            {
+                Console.WriteLine("No, he will not earn the world-cup replica ball.");
+                Console.WriteLine($"He needs {(sumNeededForBall - totalSum):F2} lv. more.");
+            }
+        }
+    }
+}
